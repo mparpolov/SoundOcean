abb435b9d9b382751b41ec0e706bf89803c0d646
mauUsatai <mauricio.usatai@gmail.com>
2020-04-23 20:28:47 -0300

    Better UI

--- frontend/src/components/Filler.js
+++ frontend/src/components/Filler.js
@@ -11,27 +11,28 @@ import '../styles/filler.css';
 
 const Filler = props => {
   const [state, dispatch] = useContext(Context);
-  const [info, setInfo] = useState('');
+  const [visible, setVisible] = useState(false);
+  const [trackName, setTrackName] = useState('');
+  const [artist, setArtist] = useState('');
 
   useEffect(() => {
-    if ( state.trackName ) {
-      // setInfo(`${state.trackName} by ${state.albumName}`);
-      setInfo(
-        <>
-          <span>Now Playing</span>
-          <h1>
-            {state.trackName} by <span className="text-highlight">{state.albumArtist}</span>
-          </h1>
-        </>
-      );
+    if ( state.playing && state.trackName && state.albumArtist ) {
+      setVisible(true);
+      setTrackName(state.trackName);
+      setArtist(state.albumArtist);
+    } else {
+      setVisible(false);
     }
-  }, [state.trackName]);
+  }, [state.playing]);
 
   return (
     <div className="panel">
       <Nav />
-      <section className="now-playing-container">
-        {info}
+      <section className={`now-playing-container ${visible ? 'visible' : 'hidden'}`}>
+        <span>Now Playing</span>
+        <h1>
+          <span className="highlight">{trackName}</span> by <span className="highlight">{artist}</span>
+        </h1>
       </section>
       <section className="albums-panel">
         {props.albums.map(album => {
